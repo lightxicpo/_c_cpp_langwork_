d2909a34536324ae1b6d940115da4518fe7be0e6
Anshu Naagar <andydowsen47us@gmail.com>
2026-06-22 01:36:35 +0530

    codebase-init

---
+++ _c_cpp_langwork_.cpp
@@ -0,0 +1,12 @@
+﻿// _c_cpp_langwork_.cpp : Defines the entry point for the application.
+//
+
+#include "_c_cpp_langwork_.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ _c_cpp_langwork_.h
@@ -0,0 +1,8 @@
+﻿// _c_cpp_langwork_.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
