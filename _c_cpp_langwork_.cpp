9b85684290a11c32fc6939a88af04c60011d2830
Anshu Naagar <andydowsen47us@gmail.com>
2026-06-25 00:33:43 +0530

    Thread stop and beep sound(experiments)

--- _c_cpp_langwork_.cpp
+++ _c_cpp_langwork_.cpp
@@ -1,10 +1,25 @@
 ﻿#include "_c_cpp_langwork_.h"
 #include "source/general_problems__.cpp"
-
+#include <stdlib.h>
+#include <thread>
+#include <Windows.h>
+#include <chrono>
 
 static inline void _pointer__arithmetic__() {
-	std::vector<int> inter_work__ = { 1, 2, 3, 4 };
-	std::cout << "Chal ja kuttee!!"; 
+	int arr_phase[1000] = { 0 };
+	int iterator_random_integer_push = 0;
+	while (iterator_random_integer_push < 1000) {
+		int random_generator__ = rand();
+		arr_phase[iterator_random_integer_push] = random_generator__;
+		std::cout << "ending crash value: " << iterator_random_integer_push << std::endl;
+		iterator_random_integer_push++;
+	} 
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000)); // sleep the thread
+	Beep(750, 323);
+	const int *first_element__ = arr_phase;
+	for (unsigned int i = 0; i < int(sizeof(arr_phase)/sizeof(arr_phase[0])); i++) {
+		std::cout << *(first_element__ + i) << std::endl; // pointer arithemetic
+	}
 }
 
 static inline void __pointer_points() {
